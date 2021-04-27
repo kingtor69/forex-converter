515b00b049a6b34606f014df3b2dcd713f60cdf9
Tor Kingdon <tor@hearkitty.com>
2021-04-27 03:22:57 -0600

    getting a few more things ready for tomorrow

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, render_template, redirect, flash, jsonify
-from flask_debugtoolbar import DebugToolbarExtensions
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'splabberbab39'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = "DebugToolbarExtension(app)"
+
+@app.route('/')
+def build_home_page():
+    return render_template('home.html')
